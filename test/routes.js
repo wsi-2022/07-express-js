4260d14e29a4f0e86d39743033c327aab07b2a93
Karl Stolley <karl.stolley@gmail.com>
2022-03-22 15:55:41 -0500

    Test for correct heading in HTML

--- test/routes.js
+++ test/routes.js
@@ -1,6 +1,7 @@
-const assert = require('assert');
+const assert = require('assert/strict');
 const supertest = require('supertest');
 const app = require('../app');
+const { JSDOM } = require('jsdom');
 
 describe('GET /', function() {
   it('should have status code 200', function(done) {
@@ -12,5 +13,14 @@ describe('GET /', function() {
     supertest(app)
       .get('/')
       .expect('Content-Type', /text\/html/, done)
+  });
+  it('should have the correct heading', async function() {
+    const response = await supertest(app)
+      .get('/');
+
+    const { document } = (new JSDOM(response.text)).window;
+
+    assert.equal(document.querySelector('h1').textContent, 'Express');
+
   });
 });
